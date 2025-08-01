81efb45219b120ed7244e98909c9e793aac93ae5
tingchi-1123 <99710250+tingchi-1123@users.noreply.github.com>
2025-08-01 18:00:20 +0800

    edit/delete

--- to-do-list/src/Components/Todo.jsx
+++ to-do-list/src/Components/Todo.jsx
@@ -1,9 +1,40 @@
-function Todo() {
-  return (
-    <div className="todo">
-      <p>洗衣服</p>
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { FaRegEdit } from "react-icons/fa";
+import EditForm from "./Editform";
+
+function Todo({
+  todo,
+  deleteTodo,
+  toggleCompleted,
+  toggleIsEditing,
+  editTodo,
+}) {
+  return todo.isEditing ? (
+    <EditForm todo={todo} editTodo={editTodo} />
+  ) : (
+    <div className={`todo ${todo.isCompleted ? "completed" : ""}`}>
+      <p
+        onClick={() => {
+          toggleCompleted(todo.id);
+        }}
+      >
+        {todo.content}
+      </p>
+      <div>
+        <FaRegEdit
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            toggleIsEditing(todo.id);
+          }}
+        />
+        <RiDeleteBin5Line
+          style={{ cursor: "pointer", marginLeft: "5px" }}
+          onClick={() => {
+            deleteTodo(todo.id);
+          }}
+        />
+      </div>
     </div>
   );
 }
-
 export default Todo;
--- to-do-list/src/Components/TodoWrappers.jsx
+++ to-do-list/src/Components/TodoWrappers.jsx
@@ -3,13 +3,75 @@ import CreateFrom from "./CreateForm";
 import Todo from "./Todo";
 
 function TodoWrappers() {
-  const [todos, setTodos] = useState(["運動", "洗衣服"]);
+  const [todos, setTodos] = useState([
+    {
+      content: "運動",
+      id: Math.random(),
+      isCompleted: false,
+      isEditing: false,
+    },
+    {
+      content: "洗衣服",
+      id: Math.random(),
+      isCompleted: false,
+      isEditing: false,
+    },
+  ]);
+  const addTodo = (content) => {
+    setTodos([...todos, { content, id: Math.random() }]);
+  };
+  const deleteTodo = (id) => {
+    setTodos(
+      todos.filter((todo) => {
+        return todo.id != id;
+      })
+    );
+  };
+
+  const toggleCompleted = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : todo;
+      })
+    );
+  };
+
+  const toggleIsEditing = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo;
+      })
+    );
+  };
+
+  const editTodo = (id, newContent) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id
+          ? { ...todo, content: newContent, isEditing: false }
+          : todo;
+      })
+    );
+  };
 
   return (
     <div className="wrapper">
       <h1>待辦事項</h1>
-      <CreateFrom />
-      <Todo />
+      <CreateFrom addTodo={addTodo} />
+      {todos.map((todo) => {
+        return (
+          <Todo
+            toggleCompleted={toggleCompleted}
+            toggleIsEditing={toggleIsEditing}
+            editTodo={editTodo}
+            todo={todo}
+            key={todo.id}
+            deleteTodo={deleteTodo}
+          />
+        );
+      })}
     </div>
   );
 }
